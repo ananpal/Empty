6da7168b7ee68adfc32b6055ce073c038fa458f5
ANAND PAL <anand.pal@mavenir.com>
2020-06-19 14:09:29 -0500

    Comments incoporated

--- Anand/june/BST_Pred_Succ.cpp
+++ Anand/june/BST_Pred_Succ.cpp
@@ -15,40 +15,42 @@ tree* findPredecessor(tree* root,int key)
 {
     if(!root)
         return NULL;
-    tree* pred = findPredecessor(root->left,key);
-    if(root->val == key)
+    tree* pred = NULL;
+    
+    if(root->val >key)
     {
-        return pred;
+        pred =findPredecessor(root->left,key);
     }
-    pred = root;
-    if(root->right)
+    else if(root->val == key)
+    {
+        if(root->left)
+            return root->left;
+    }
+    else if(root->right)
     {
         pred =findPredecessor(root->right,key);
+    }
+    else
+    {
+        pred = root;
     }
     return pred;
 }
 
-
-tree* findSuccessor(tree* root,int key,bool& found=false)
+tree* findSuccessor(tree* root,int key,bool found=false)
 {
     if(!root)
         return NULL;
-    tree* left = findSuccessor(root->left,key,found);
-    if(found)
-    {
-        found = false;
+    tree* suck = findPredecessor(root->left,key,found);
+    if(found && suck)
+        return suck;
+    else if (found)
         return root;
-    }
     if(root->val == key) found =true;
-    return left||findSuccessor(root->right,key,found);
+    return findPredecessor(root->right,key,found);
 }
 
-tree* findSuccessor(tree* root,int key)
-{
-    bool found = false;
-    return findSuccessor(root,key,found);
 
-}
 main()
 {
     //BST pointer given
